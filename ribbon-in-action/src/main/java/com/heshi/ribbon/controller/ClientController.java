d16c21096f1693eea11f0eeb6c020f0b07e2f286
fukun <heshi_fk@163.com>
2019-12-29 21:24:07 +0800

    增加nacos工程

--- ribbon-in-action/src/main/java/com/heshi/ribbon/controller/ClientController.java
+++ ribbon-in-action/src/main/java/com/heshi/ribbon/controller/ClientController.java
@@ -2,10 +2,14 @@ package com.heshi.ribbon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 /**
  * @author by fukun
@@ -15,7 +19,8 @@ public class ClientController {
 
     @Autowired
     private RestTemplate restTemplate;
-
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     @Value("${server.provider.name}")
     private String serverProviderName;
 
@@ -23,5 +28,21 @@ public class ClientController {
     public String index() {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://" + serverProviderName + "/v1/hello", String.class);
         return responseEntity.getBody();
+    }
+
+    @GetMapping("/hello1")
+    public String hello1() throws IOException {
+        ServiceInstance serviceInstance = loadBalancerClient.choose(serverProviderName);
+        return loadBalancerClient.execute(serverProviderName, serviceInstance, instance -> {
+            String host = instance.getHost();
+            int port = instance.getPort();
+            String urlMethod = "http";
+            if (instance.isSecure()) {
+                urlMethod = "https";
+            }
+            String url = urlMethod + "://" + host + ":" + port + "/v1/hello";
+            RestTemplate restTemplateInner = new RestTemplate();
+            return restTemplateInner.getForEntity(url, String.class).getBody();
+        });
     }
 }
